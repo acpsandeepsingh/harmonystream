37b9a4856803ccf8efc456b82a3abe54ba7e5891
acpsandeepsingh <acpsandeepsingh@gmail.com>
2026-02-12 14:45:43 +0530

    Update use-previous.ts

--- src/hooks/use-previous.ts
+++ src/hooks/use-previous.ts
@@ -3,9 +3,11 @@
 import { useEffect, useRef } from 'react';
 
 export function usePrevious<T>(value: T) {
-  const ref = useRef<T>();
+  const ref = useRef<T | undefined>(undefined);
+  
   useEffect(() => {
     ref.current = value;
   });
+  
   return ref.current;
 }
