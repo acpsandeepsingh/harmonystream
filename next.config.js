f480e877b010b23c2e2a3bf5f507ec2db5f90237
acpsandeepsingh <acpsandeepsingh@gmail.com>
2026-02-13 11:14:36 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Required for static export
   output: 'export',
+
+  // Required for deploying to a subdirectory on GitHub Pages
   basePath: '/harmonystream',
+
+  // Required for static export
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
 };
 
 module.exports = nextConfig;
