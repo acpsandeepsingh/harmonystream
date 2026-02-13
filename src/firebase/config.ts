33ba4dc6541fc9b910ccbc85a1dce4111b771bb1
acpsandeepsingh <acpsandeepsingh@gmail.com>
2026-02-13 22:22:14 +0530

    Update Firebase configuration values
    Updated Firebase appId and apiKey for configuration.

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-8258640677-40b59",
-  "appId": "1:312831088099:web:375cb5be170cf833033cd6",
-  "apiKey": "AIzaSyD6g0OuFO3_KhVtiDmTc3ks-KclZU-6In0",
+  "appId": "1:312831088099:web:e9129a94070e9cff033cd6",
+  "apiKey": "AIzaSyAbHGZu-NdtvQhV_IkWh0htVgsSoIHam-s",
   "authDomain": "studio-8258640677-40b59.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "312831088099"
