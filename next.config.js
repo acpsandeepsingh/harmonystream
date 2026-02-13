7109d581a5d37776c0f7e32b835ad7b04c85127d
acpsandeepsingh <acpsandeepsingh@gmail.com>
2026-02-13 21:14:31 +0530

    Refactor next.config.js by removing exportPathMap

--- next.config.js
+++ next.config.js
@@ -1,28 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: { unoptimized: true },
-  trailingSlash: true,
-  basePath: '/harmonystream',
-  assetPrefix: '/harmonystream',
-  
-  // Explicitly define all routes to export
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      '/': { page: '/' },
-      '/search': { page: '/search' },
-      '/library': { page: '/library' },
-      '/history': { page: '/history' },
-      '/profile': { page: '/profile' },
-      '/settings': { page: '/settings' },
-      '/login': { page: '/login' },
-      '/signup': { page: '/signup' },
-      '/404': { page: '/404' },
-    }
-  },
-};
-
-module.exports = nextConfig
+ const nextConfig = {
+   output: 'export',
+   images: { unoptimized: true },
+   trailingSlash: true,
+   basePath: '/harmonystream',
+   assetPrefix: '/harmonystream',
+ };
+ 
+ module.exports = nextConfig;
