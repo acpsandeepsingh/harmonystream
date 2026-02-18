cfde18044b0301954ad3e7fec74b92e042eb7af9
acpsandeepsingh <acpsandeepsingh@gmail.com>
2026-02-18 12:55:04 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,24 +1,25 @@
 /** @type {import('next').NextConfig} */
-
-// Detect if building for Android
-const isAndroidBuild = process.env.BUILD_TARGET === 'android';
-
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
   trailingSlash: true,
-  // Relative assets are required for the standalone Android WebView build.
-  ...(isAndroidBuild && {
-    assetPrefix: './',
-  }),
+  basePath: '/harmonystream',
+  assetPrefix: '/harmonystream',
   
-  // Only use basePath for web builds (GitHub Pages)
-  // NOT for Android builds
-  ...(!isAndroidBuild && {
-    basePath: '/harmonystream',
-    assetPrefix: '/harmonystream',
-  }),
-  
+  exportPathMap: async function () {
+    return {
+      '/': { page: '/' },
+      '/search': { page: '/search' },
+      '/library': { page: '/library' },
+      '/library/playlist': { page: '/library/playlist' },
+      '/history': { page: '/history' },
+      '/profile': { page: '/profile' },
+      '/settings': { page: '/settings' },
+      '/login': { page: '/login' },
+      '/signup': { page: '/signup' },
+      '/404': { page: '/404' },
+    };
+  },
 };
 
 module.exports = nextConfig;
