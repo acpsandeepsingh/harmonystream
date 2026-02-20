23c92fc7c86da90711f9664ed760eccf8da7632c
acpsandeepsingh <acpsandeepsingh@gmail.com>
2026-02-20 14:39:46 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,20 +1,25 @@
 /** @type {import('next').NextConfig} */
+
 const WEB_BASE_PATH = '/harmonystream';
+
+// Detect if building for Android
 const isAndroidBuild = process.env.BUILD_TARGET === 'android';
 
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
   trailingSlash: true,
-  
-  // Website: Use basePath
+
+  // Keep website hosted under the project subpath.
   ...(!isAndroidBuild && {
     basePath: WEB_BASE_PATH,
     assetPrefix: WEB_BASE_PATH,
   }),
-  
-  // Android: NO assetPrefix! 
-  // Remove the assetPrefix: './' line completely!
+
+  // Relative assets are required for the standalone Android WebView build.
+  ...(isAndroidBuild && {
+    assetPrefix: './',
+  }),
 };
 
 module.exports = nextConfig;
