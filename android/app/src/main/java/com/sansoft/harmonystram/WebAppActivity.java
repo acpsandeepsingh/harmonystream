9e31d79867155321144a35203a2a491677309605
acpsandeepsingh <acpsandeepsingh@gmail.com>
2026-02-22 12:39:27 +0530

    Update WebAppActivity.java

--- android/app/src/main/java/com/sansoft/harmonystram/WebAppActivity.java
+++ android/app/src/main/java/com/sansoft/harmonystram/WebAppActivity.java
@@ -232,22 +232,12 @@ public class WebAppActivity extends AppCompatActivity {
         }
         dispatchPendingMediaAction(intent.getStringExtra(PlaybackService.EXTRA_PENDING_MEDIA_ACTION));
     }
+
     @Override
-    protected void onPause() {
-        super.onPause();
-        webView.onPause();
-        // NO pauseTimers() - this allows background audio
-        
-    }
-    @Override
-        protected void onResume() {
+    protected void onResume() {
         super.onResume();
         // Recovery hook: after lock-screen/app resume, ask web player to re-assert playback if needed.
-        
-        webView.onResume();
-        webView.resumeTimers();
-		dispatchToWeb("window.dispatchEvent(new CustomEvent('nativeHostResumed'));\n");
-		
+        dispatchToWeb("window.dispatchEvent(new CustomEvent('nativeHostResumed'));\n");
     }
 
     private void dispatchPendingMediaAction(String action) {
